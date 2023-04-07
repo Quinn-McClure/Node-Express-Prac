976dca0521fd13ceef2ec7246149921bb46429cb
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-07 08:15:26 -0500

    added 1 server no routing init

--- server-and-routing/server1.js
+++ server-and-routing/server1.js
@@ -1,2 +1,11 @@
 const http = require('http');
 const url = require('url');
+
+const server = http.createServer(function (req, res) {
+    console.log('hello');
+    res.end('hello from server');
+});
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening on port 3000');
+});
