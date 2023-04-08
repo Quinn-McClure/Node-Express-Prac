34b2ab3cd8d84720932b1912a22885b4dc2c1bcc
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-08 10:41:46 -0500

    added basic routing to my own server

--- server-and-routing/server1.js
+++ server-and-routing/server1.js
@@ -2,8 +2,19 @@ const http = require('http');
 const url = require('url');
 
 const server = http.createServer(function (req, res) {
-    console.log('hello');
-    res.end('hello from server');
+    const pathName = req.url;
+
+    if(pathName === '/') {
+        res.end('THis is the home page');
+    }
+    else if(pathName === '/movies') {
+        res.end('THis is the movies page');
+    }
+    else {
+        res.end('page not found')
+    }
+
+    console.log(pathName);
 });
 
 server.listen(3000, 'localhost', () => {
