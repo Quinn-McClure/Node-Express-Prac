a944f839377ae58a53413a159543409858e258fd
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-08 19:33:10 -0500

    more on server2

--- server-and-routing/server2.js
+++ server-and-routing/server2.js
@@ -1,8 +1,26 @@
 const http = require('http');
+const path = require('path');
 const url = require('url');
 
 const server = http.createServer(function(req, res) {
+    const pathName = req.url;
 
+    if(pathName === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end('<h1>Home</h1>');
+    }
+    else if(pathName === '/bio') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.end('<h1>My bio</h1>');
+    }
+    else {
+        res.end('Page Not Found');
+        console.log(req.url);
+    }
 });
 
 server.listen(3000, 'localhost', () => {
