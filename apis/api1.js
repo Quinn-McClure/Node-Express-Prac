aeb71753edde78797498a65742bcc7b737c58963
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-13 09:01:57 -0500

    wrote own simple api

--- apis/api1.js
+++ apis/api1.js
@@ -1,21 +1,17 @@
-//server things
 const http = require('http');
-//router things (from scratch)
 const url = require('url');
 const fs = require('fs');
+
+const data = fs.readFileSync(`data.json`, 'utf-8');
+const prodData = JSON.parse(data);
 
 const server = http.createServer(function (req, res) {
     const pathName = req.url;
 
     if(pathName === '/') {
-        res.end('home');
-        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
-            const prodData = JSON.parse(data);
-            //console.log(prodData);
-            //specify json as type
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.end(data);
-        });
+        //res.end('home');
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(data);
     }
 });
 
