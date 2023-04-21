b45e5dec0f164b628d1e60c90f57a866cfb62857
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2023-04-21 08:32:37 -0500

    assembling backend

--- template/overview.js
+++ template/overview.js
@@ -3,13 +3,21 @@ const url = require('url');
 const fs = require('fs');
 
 //stuff that gets loaded when the server is loaded
+const replaceTemplate = (temp, item) => {
+    let output = output.replace(/{%IMAGE%}/g, item.image);
+
+    return output
+}
+
+const page = fs.readFileSync(`${__dirname}/overview.html`);
+
 
 //server routes
 const server = http.createServer(function(req, res) {
     const pathName = req.url;
 
     if(pathName === '/') {
-        
+        res.writeHead(200, {'Content-Type': 'text/html'});
     }
 });
 
